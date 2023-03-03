dd40b6301096d003786a7033ec6db1cca7e4f677
ky13 <ky13@ky13.buildbox.ansible.com>
2023-03-02 20:37:35 -0500

    initial swagger int

---
+++ docs.go
@@ -0,0 +1,16 @@
+//  ky13buildbox api:
+//   version: v1.0
+//   title: ky13buildbox api
+//  Schemes: http, https
+//  Host: 192.168.1.206:8080
+//  BasePath: /
+//  Produces:
+//    - application/json
+//
+// securityDefinitions:
+//  apiKey:
+//    type: apiKey
+//    in: header
+//    name: authorization
+// swagger:meta
+package main
--- main.go
+++ main.go
@@ -3,32 +3,58 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/superky13/ky13BuildBoxApi/serverlogger"
 	"log"
 	"net/http"
+
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/superky13/ky13BuildBoxApi/serverlogger"
 )
 
+// swagger:parameters admin serverinfo
 type serverinfo struct {
-	ID         string `json="id"`
+	// name: id
+	// in: path
+	// type: string
+	// required: true
+	ID string `json="id"`
+	// name: servername
+	// in: path
+	// type: string
+	// required: true
 	Servername string `json="servername"`
-	Ipaddress  string `json="ipaddress"`
+	// name: ipaddress
+	// in: path
+	// type: string
+	// required: true
+	Ipaddress string `json="ipaddress"`
 }
 
 var servers []serverinfo
 
 func returnServerInfo(w http.ResponseWriter, r *http.Request) {
-	// fmt.Println("/api/v1/serverinfo", time.Now(), http.StatusOK)
 	serverlogger.Appendtoserverlog("/api/v1/serverinfo ")
 	json.NewEncoder(w).Encode(servers)
 }
 func apiWelcome(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "ky13 buildbox api server")
 	serverlogger.Appendtoserverlog("/api/v1 ")
-	// log.Output(1, http.StatusText(200))
 }
-func handleRequests() {
+
+// swagger:route GET /api/v1 admin handleApiRequests
+//
+// security:
+// - apiKey: []
+// responses:
+// 200:
+// 400:
+func handleApiRequests() {
 	http.HandleFunc("/api/v1", apiWelcome)
 	http.HandleFunc("/api/v1/serverinfo", returnServerInfo)
+	// developer swagger docs
+	http.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
+	opts := middleware.SwaggerUIOpts{SpecURL: "/swagger.yaml"}
+	sh := middleware.SwaggerUI(opts, nil)
+	http.Handle("/docs", sh)
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
 func main() {
@@ -36,5 +62,5 @@ func main() {
 		serverinfo{ID: "1", Servername: "dns.ky13buildbox.com", Ipaddress: "192.168.122.120"},
 		serverinfo{ID: "2", Servername: "git.ky13buildbox.com", Ipaddress: "192.168.122.189"},
 	}
-	handleRequests()
+	handleApiRequests()
 }
